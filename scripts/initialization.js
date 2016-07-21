c9f791827328cf11423903ee07c5a9502ad54cfe
Nic Nash <nicnash08@gmail.com>
2016-07-21 15:21:54 -0500

    nothing

--- scripts/initialization.js
+++ scripts/initialization.js
@@ -67,9 +67,9 @@ var VEHICLE_TYPE_TO_IMG = {
 
 var pollrate = 2455000;
 // var url = 'https://exelon-api.herokuapp.com/v1/';
-var url = 'https://exelon-api-staging.herokuapp.com/v1/';
+// var url = 'https://exelon-api-staging.herokuapp.com/v1/';
 // var url = 'https://exelon-api-qa.herokuapp.com/v1/';
-// var url = 'https://exelon-api-production.herokuapp.com/v1/';
+var url = 'https://exelon-api-production.herokuapp.com/v1/';
 var siteId = 1; //limerick
 var aheadOrBehindPct = .25;  // if behind .25 estimated time its longer.   if ahead by .25.. its faster
 var titleHeight = 83;
